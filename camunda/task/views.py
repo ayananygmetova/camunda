db4803cd895c0227bcf2c4cb6214a731c2f4321a
beketx <beketsk@gmail.com>
2020-09-08 23:53:56 +0600

    Dev

--- camunda/task/views.py
+++ camunda/task/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from task.models import Task
+from task.serializers import TaskSerializer
+
+
+class Tasks(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name')
+
+class TasksByUserIDAPIView(APIView):
+    def get(self, request, user_id):
+        task = Task.objects.filter(user_id=user_id)
+        serializer = TaskSerializer(task, many=True)
+        return Response(serializer.data)
